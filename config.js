e32f3428f63be48194677cb572310406c4a9aca9
dashmeet-singh <51048832+dashmeet-singh@users.noreply.github.com>
2019-05-26 23:29:11 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 var config = {
   dev: {
-      dbURL: 'mongodb+srv://user:user@cluster0-nhnjo.mongodb.net/test?retryWrites=true'
+      dbURL: 'mongodb+srv://user:user@cluster0-nhnjo.mongodb.net/test?retryWrites=true',
+      ENDPOINT : {
+        CUSTOMER_TRANSACTIONS : 'http://localhost:8080/customer-transactions',
+        ADD_TRANSACTION : 'http://localhost:8080/add-transaction'
+      }
   },
   prod: {
-      dbURL: 'mongodb+srv://user:user@cluster0-nhnjo.mongodb.net/test?retryWrites=true'
+      dbURL: 'mongodb+srv://user:user@cluster0-nhnjo.mongodb.net/test?retryWrites=true',
+      ENDPOINT : {
+        CUSTOMER_TRANSACTIONS : 'http://localhost:8080/customer-transactions',
+        ADD_TRANSACTION : 'http://localhost:8080/add-transaction'
+      }
   }
 };
 
--- redux/actions/transactionActions.js
+++ redux/actions/transactionActions.js
@@ -1,11 +1,14 @@
 import { FETCH_TRANSACTIONS, TRANSFER_TRANSACTION, SET_PROGRESS_BAR, SET_FORM_PROGRESS_BAR} from './types';
 import axios from 'axios'
 import { toast } from "react-toastify";
+import config from "../../config";
+
+const ENDPOINT = config['dev'].ENDPOINT;
 
 export const fetchTransactions = () => dispatch => {
     console.log('fetching transactions');
     dispatch({type : SET_PROGRESS_BAR, payload : true});
-    axios.get(`http://localhost:8080/customer-transactions`)
+    axios.get(ENDPOINT.CUSTOMER_TRANSACTIONS)
         .then(res => {
           dispatch({
             type : FETCH_TRANSACTIONS,
@@ -25,7 +28,7 @@ export const transferTransaction = (trxnData) => dispatch => {
    console.log('transferring transaction');
    dispatch({type : SET_FORM_PROGRESS_BAR, payload : true});
 
-   axios.post('http://localhost:8080/add-transaction', trxnData)
+   axios.post(ENDPOINT.ADD_TRANSACTION, trxnData)
         .then(function (response) {
               console.log(response);
               toast.success("Transaction success - Amount succesfully transferred!");
